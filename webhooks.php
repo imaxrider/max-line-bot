bc7ee4df5f39ea11979eb6284a094dea94cdd5ea
iMaX Rider <kwampien@gmail.com>
2022-08-31 00:59:07 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,7 +1,7 @@
 <?php
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = 'E5Xpjy7n9oJYDePp2C+hL0TjfpyPOFIc1jpw334NaQblRH0P9sFfw3U4C4w7JilPJoJwApWWg7hobueuo+4KkvvJdHrW2gSMf2rspP01DyF1mZhqEfGGQV5VfHWzooEon9tMURMa0MANiHxm95LplAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'access_token';
 
 // Get POST body content
 $content = file_get_contents('php://input');
